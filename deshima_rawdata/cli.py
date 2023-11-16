a8281c7d864c43919a1e0106640ec001d9ea936a
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-11-16 18:36:18 +0000

    #9 Add list command

--- deshima_rawdata/cli.py
+++ deshima_rawdata/cli.py
@@ -1,4 +1,4 @@
-__all__ = ["download"]
+__all__ = ["download", "list"]
 
 
 # standard library
@@ -7,6 +7,7 @@ from pathlib import Path
 
 
 # dependencies
+import pandas as pd
 from fire import Fire
 from requests import get
 from tqdm import tqdm
@@ -15,8 +16,14 @@ from . import __version__
 
 # constants
 CHUNK_SIZE = 1024
-DEFAULT_TAG = f"v{__version__}"
-GITHUB_URL = "https://raw.githubusercontent.com/deshima-dev/rawdata"
+DATA_LIST = pd.read_csv(
+    Path(__file__).with_name("data.csv"),
+    index_col=0,
+    dtype={0: str},
+)
+DATA_REPO_URL = "https://github.com/deshima-dev/rawdata"
+DEFAULT_DATA_TAG = f"v{__version__}"
+DEFAULT_LIST_FORMAT = "markdown"
 
 
 def download(
@@ -26,7 +33,7 @@ def download(
     dir: Path = Path(),
     extract: bool = False,
     progress: bool = False,
-    tag: str = DEFAULT_TAG,
+    tag: str = DEFAULT_DATA_TAG,
 ) -> Path:
     """Download DESHIMA raw data for given observation ID.
 
@@ -41,7 +48,8 @@ def download(
         Path of the downloaded raw data.
 
     """
-    url = f"{GITHUB_URL}/{tag}/data/{obsid}.tar.gz"
+    file_name = Path(DATA_LIST["File name"][str(obsid)])
+    url = f"{DATA_REPO_URL}/raw/{tag}/data/{file_name}"
 
     if not (response := get(url, stream=True)).ok:
         response.raise_for_status()
@@ -52,7 +60,7 @@ def download(
         "unit": "B",
         "unit_scale": True,
     }
-    data_path = Path(dir) / response.url.split("/")[-1]
+    data_path = Path(dir) / file_name
 
     with tqdm(**bar_options) as bar, open(data_path, "wb") as f:
         for data in response.iter_content(CHUNK_SIZE):
@@ -67,9 +75,22 @@ def download(
         dir_name = tar.getnames()[0]
 
     data_path.unlink(True)
-    return data_path.parent / dir_name
+    return data_path.with_name(dir_name)
+
+
+def list(format: str = DEFAULT_LIST_FORMAT) -> str:
+    """List DESHIMA raw datasets available in the package.
+
+    Args:
+        format: Format of the list that can be output by pandas.
+
+    Returns:
+        String of the list with given format.
+
+    """
+    return getattr(DATA_LIST, f"to_{format}")()
 
 
 def main() -> None:
     """Entry point of the deshima-rawdata command."""
-    Fire({"download": download})
+    Fire({"download": download, "list": list})
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,2 +1,18 @@
+# standard library
+from pkgutil import get_data
+
+
+# dependencies
+from deshima_rawdata import cli
+
+
+# constants
+DATA_LIST = get_data("deshima_rawdata", "data.csv")
+
+
 def test_download() -> None:
     pass
+
+
+def test_cli_list() -> None:
+    assert cli.list("csv") == DATA_LIST.decode()
